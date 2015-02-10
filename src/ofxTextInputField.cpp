5e3589e117036d59cb94e2619ae3c4d3ce753cda
Andreas Borg <communic@tion.to>
2015-02-10 16:55:22 -0500

    Fix for misplaced define clause

--- src/ofxTextInputField.cpp
+++ src/ofxTextInputField.cpp
@@ -323,12 +323,7 @@ void ofxTextInputField::mouseReleased(ofMouseEventArgs& args){
 }
 
 
-#ifdef OF_VERSION_MINOR
-#if OF_VERSION_MINOR>=8 || OF_VERSION_MAJOR>0
-#define USE_GLFW_CLIPBOARD
 
-#endif
-#endif
 
 
 #ifdef USE_GLFW_CLIPBOARD
